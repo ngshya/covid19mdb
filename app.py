cc7758b27f2664401696ba8f32fd08d149248d87
ngshya <ngshya@gmail.com>
2020-04-06 01:54:28 +0200

    remove main

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from covid19mdb.flask import create_app
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=5000)
+
+app = create_app()
+app.run(host="0.0.0.0", port=5000)
